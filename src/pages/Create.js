fb2d4d8f1f2356c66d2ef9abf5483ec616a3f4c5
Nasuri <victorbenedict.bulaong@gmail.com>
2023-12-15 22:50:34 +0800

    Setup database

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,64 @@
+import { useState } from "react"
+import supabase from "../config/supabaseClient"
+
 const Create = () => {
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [rating, setRating] = useState('')
+  const [formError, setFormError] = useState(null)
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    
+    if(!title || !method || !rating) {
+      setFormError('Please fill in all the field correctly')
+      return
+    }
+
+    const { data, error } = await supabase
+      .from('smoothies')
+      .insert([{ title, method, rating }])
+
+    if (error) {
+      console.log(error)
+      setFormError('Please fill in all the field correctly')
+    }
+    if (data) {
+      console.log(data)
+      setFormError(null)
+    }
+
+  }
+
   return (
     <div className="page create">
-      <h2>Create</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor="method">Method:</label>
+        <textarea
+          id="method"
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+        <label htmlFor="rating">Rating:</label>
+        <input
+          type="number"
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <button>Create Smooth Recipe</button>
+
+        {formError && <p className="error">{formError}</p>}
+      </form>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,48 @@
+import supabase from "../config/supabaseClient"
+import { useEffect, useState } from "react"
+
+//components 
+import SmoothieCard from "../components/SmoothieCard"
+
 const Home = () => {
+  console.log(supabase)
+  const [fetchError, setFetchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+
+  useState(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+        .from('smoothies')
+        .select()
+        
+        if(error) {
+          setFetchError('Could not fetch the smoothies')
+          setSmoothies(null)
+          console.log(error)
+        }
+        if(data) {
+          setSmoothies(data)
+          setFetchError(null)
+        }
+    }
+    
+    fetchSmoothies()
+
+  }, [])
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && (<p>{fetchError}</p>)}
+      {smoothies && (
+        <div className="smoothies">
+          {/* order-by buttons */}
+          <div className="smoothie-grid">
+            {smoothies.map(smoothie => (
+              <SmoothieCard key={smoothie.id} smoothie={smoothie}/>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
